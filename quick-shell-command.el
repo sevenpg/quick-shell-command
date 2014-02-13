eebfbef7ee61f46b0bd0fb055690b5a5ea4aa707
Kohei Tsuruta <kohei.tsuruta@gree.net>
2014-02-13 13:54:43 -0800

    Import

---
+++ quick-shell-command.el
@@ -0,0 +1,22 @@
+(defvar quick-shell-command "" "Command that gets executed by quick-shell-command")
+
+(defun quick-shell-command-set (&optional command)
+  (if (stringp command)
+      (setq quick-shell-command command)
+    (let ((def-value ""))
+      (if (stringp quick-shell-command)
+          (setq def-value quick-shell-command))
+      (setq quick-shell-command (read-string "Command: " def-value)))))
+
+(defun quick-shell-command (&optional prefix)
+  (interactive "P")
+  (cond ((or prefix
+             (not (stringp quick-shell-command))
+             (equal quick-shell-command ""))
+         (quick-shell-command-set)
+         ))
+
+  (save-buffer)
+  (shell-command quick-shell-command))
+
+(provide 'quick-shell-command)
